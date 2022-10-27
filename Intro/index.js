ef0371e36c1330bf447e917ad60df6c5ae4bc907
sharkeeyy <sfmei2003@yandex.ru>
2022-10-27 13:33:52 +0300

    changing color

--- Intro/index.js
+++ Intro/index.js
@@ -1,5 +1,5 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth -100, window.innerHeight - 100);
@@ -11,21 +11,37 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 4;
+camera.position.x = 0;
 
-let speed = 0.10;
+// let speedX = 0.05;
+let speedZ = 0.05;
 
 function animate() {
 	requestAnimationFrame( animate );
 
   cube.rotation.x += 0.02;
   cube.rotation.y += 0.02;
-  camera.position.z += speed;
 
-  if (camera.position.z > 10) speed = -0.05;
-  if (camera.position.z < 1) speed = 0.05;
+  camera.position.z += speedZ;
+  
 
+  if (camera.position.z > 20) {
+    let color = getRandomColor();
+    cube.material.color.setHex(color);
+    speedZ = -0.05;
+  }
+  if (camera.position.z < 1) {
+    let color = getRandomColor();
+    cube.material.color.setHex(getRandomColor());
+    speedZ = 0.05;
+  }
   
 
 	renderer.render( scene, camera );
 }
+
+function getRandomColor() {
+  return '0x' + Math.floor(Math.random() * 16777216).toString(16);
+}
+
 animate();
